156b4e2d3ce100cf7466ec92dddd27cb0175f9ff
UncleBikki <nickcummings1994@gmail.com>
2021-12-06 00:19:00 -0600

    added click funtion

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,17 @@
 var CurrentDay = document.querySelector("#currentDay")
 var Today = moment();
 
-CurrentDay.textContent = Today.format("dddd, MMMM Do YYYY, h:mm:ss a");
+CurrentDay.textContent = Today.format("dddd, MMMM Do YYYY");
 
+//i need a click listener for the save button
+$(".saveBtn").on("click", function() {
+    //set variables for text and time
+    event.preventDefault();
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
+    //save the text in local storage
+    localStorage.setItem(text, time);
+});
 
+//now i need it to persist after
